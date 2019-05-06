b831e9f6d2fdb645a371db72c5ff5218c62df964
pkrueger <philip.krueger@caci.com>
2019-05-06 14:05:01 -0700

    Add project files.

---
+++ GettysburgStringApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettysburgStringApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
